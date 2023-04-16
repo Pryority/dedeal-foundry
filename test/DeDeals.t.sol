88042b4b63610684f468d69dec0f309fcc698f25
pryority <matthewapryor@gmail.com>
2023-04-16 14:36:28 -0400

    update: tests

--- test/DeDeals.t.sol
+++ test/DeDeals.t.sol
@@ -6,14 +6,28 @@ import "../src/DeDeals.sol"; // Import the DeDeals contract
 
 contract DeDealsTest is Test {
     DeDeals dedeals; // Declare a variable to hold the DeDeals contract instance
+    mapping(uint256 => Deal) dealIdMap;
+    uint256 executedBalance;
+
+    struct Deal {
+        uint256 dealId;
+        address sellerAddress;
+        address buyerAddress;
+        uint256 sellerDeposit;
+        uint256 buyerDeposit;
+        uint256 dealAmount;
+        uint256 depositReleaseTime;
+        uint256 grantDeadline;
+        uint256 executeDeadlineInterval;
+    }
 
     function setUp() public {
         dedeals = new DeDeals(); // Deploy a new instance of the DeDeals contract
     }
 
     function test_GetDeal() public payable {
-        // Call the claim function with the necessary parameters
-        DeDeals.Deal memory deal = dedeals.claim(100, 3600);
+        // Call the createDeal function with the necessary parameters
+        DeDeals.Deal memory deal = dedeals.createDeal(100, 3600);
 
         // Call the getDeal function with the dealId and assert that the returned deal has the expected values
         DeDeals.Deal memory retrievedDeal = dedeals.getDeal(deal.dealId);
@@ -22,14 +36,14 @@ contract DeDealsTest is Test {
         assertEq(retrievedDeal.buyerAddress, address(0));
         assertEq(retrievedDeal.sellerDeposit, msg.value);
         assertEq(retrievedDeal.buyerDeposit, 0);
-        assertEq(retrievedDeal.amountOfClaim, msg.value);
+        assertEq(retrievedDeal.dealAmount, msg.value);
         assertEq(retrievedDeal.grantDeadline, block.timestamp + 99);
         assertEq(retrievedDeal.executeDeadlineInterval, 3600);
     }
 
-    function test_Claim() public payable {
-        // Call the claim function with the necessary parameters
-        DeDeals.Deal memory deal = dedeals.claim(100, 3600);
+    function test_CreateDeal() public payable {
+        // Call the createDeal function with the necessary parameters
+        DeDeals.Deal memory deal = dedeals.createDeal(100, 3600);
 
         // Assert that the returned deal has the expected values
         assertEq(deal.dealId, 1);
@@ -37,41 +51,91 @@ contract DeDealsTest is Test {
         assertEq(deal.buyerAddress, address(0));
         assertEq(deal.sellerDeposit, msg.value);
         assertEq(deal.buyerDeposit, 0);
-        assertEq(deal.amountOfClaim, msg.value);
+        assertEq(deal.dealAmount, msg.value);
         assertEq(deal.grantDeadline, block.timestamp + 99);
         assertEq(deal.executeDeadlineInterval, 3600);
     }
 
     function test_GetDeal_InvalidDealId() public payable {
-        (bool success,) = address(dedeals).call(abi.encodeWithSignature("getDeal(uint256)", 100));
+        (bool success, ) = address(dedeals).call(
+            abi.encodeWithSignature("getDeal(uint256)", 100)
+        );
+        assertEq(success, false);
         vm.expectRevert("INVALID_DEAL_ID");
         dedeals.getDeal(101);
     }
 
-    function test_Claim_InsufficientDeposit() public payable {
-        // Call the claim function with an insufficient deposit and assert that it reverts
-        (bool success,) = address(dedeals).call{value: 50}(abi.encodeWithSignature("claim(uint256,uint256)", 100, 3600));
-        assertTrue(success, "claim should revert with insufficient deposit");
-        vm.expectRevert("Insufficient deposit for claim");
-        address(dedeals).call{value: 50}(abi.encodeWithSignature("claim(uint256,uint256)", 100, 3600));
+    // function test_BuyerExecuteSeller_ValidDealId() public payable {
+    //     vm.startPrank(address(dedeals));
+    //     // Call the createDeal function with the necessary parameters
+    //     DeDeals.Deal memory deal = dedeals.createDeal(100, 3600);
+
+    //     // Set the buyer address for the deal
+    //     deal.buyerAddress = address(this);
+
+    //     // Set the seller deposit for the deal
+    //     deal.sellerDeposit = 100;
+
+    //     // Call the buyerExecuteSeller function with the valid _dealId and correct buyer and msg.value
+    //     (bool success, ) = address(dedeals).call{value: 200}(
+    //         abi.encodeWithSignature("buyerExecuteSeller(uint256)", deal.dealId)
+    //     );
+    //     assertTrue(success, "buyerExecuteSeller should execute successfully");
+
+    //     // Assert that the contract state is updated as expected
+    //     assertEq(dealIdMap[deal.dealId].sellerDeposit, 0);
+    //     assertEq(dealIdMap[deal.dealId].buyerDeposit, 200);
+    //     assertEq(
+    //         dealIdMap[deal.dealId].depositReleaseTime,
+    //         block.timestamp + deal.executeDeadlineInterval
+    //     );
+    //     assertEq(executedBalance, 100);
+
+    //     // Assert that the emitted event has the correct values
+    //     assertEq(dealIdMap[deal.dealId].sellerAddress, deal.sellerAddress);
+    //     assertEq(dealIdMap[deal.dealId].buyerAddress, deal.buyerAddress);
+    //     assertEq(dealIdMap[deal.dealId].sellerDeposit, deal.sellerDeposit);
+    //     assertEq(dealIdMap[deal.dealId].buyerDeposit, deal.buyerDeposit);
+    //     assertEq(dealIdMap[deal.dealId].dealAmount, deal.dealAmount);
+    //     assertEq(dealIdMap[deal.dealId].grantDeadline, deal.grantDeadline);
+    //     assertEq(
+    //         dealIdMap[deal.dealId].executeDeadlineInterval,
+    //         deal.executeDeadlineInterval
+    //     );
+    //     vm.stopPrank();
+    // }
+
+    function test_CreateDeal_InsufficientDeposit() public payable {
+        // Call the createDeal function with an insufficient deposit and assert that it reverts
+        (bool success, ) = address(dedeals).call{value: 50}(
+            abi.encodeWithSignature("createDeal(uint256,uint256)", 100, 3600)
+        );
+        assertTrue(
+            success,
+            "createDeal should revert with insufficient deposit"
+        );
+
+        vm.expectRevert("Insufficient deposit for createDeal");
+        (bool createDeal, ) = address(dedeals).call{value: 50}(
+            abi.encodeWithSignature("createDeal(uint256,uint256)", 100, 3600)
+        );
+        assertFalse(createDeal);
     }
 
-    function test_Claim_ExceededGrantDeadline() public payable {
+    function test_CreateDeal_ExceededGrantDeadline() public payable {
         vm.roll(10);
 
-        // Call the claim function and assert that it reverts
-        (bool success,) =
-            address(dedeals).call{value: 100}(abi.encodeWithSignature("claim(uint256,uint256)", 100, 3600));
-        assertTrue(success, "claim should revert after grant deadline");
+        // Call the createDeal function and assert that it reverts
+        (bool success, ) = address(dedeals).call{value: 100}(
+            abi.encodeWithSignature("createDeal(uint256,uint256)", 100, 3600)
+        );
+        assertTrue(success, "createDeal should revert after grant deadline");
     }
 
-    function test_Claim_WithExistingDeal() public payable {
-        // Call the claim function with the necessary parameters
-        DeDeals.Deal memory deal1 = dedeals.claim(100, 3600);
-
-        // Call the claim function again and assert that it reverts
-        (bool success,) =
-            address(dedeals).call{value: 100}(abi.encodeWithSignature("claim(uint256,uint256)", 100, 3600));
-        assertTrue(success, "claim should revert with existing deal");
+    function test_CreateDeal_WithExistingDeal() public payable {
+        (bool success, ) = address(dedeals).call{value: 100}(
+            abi.encodeWithSignature("createDeal(uint256,uint256)", 100, 3600)
+        );
+        assertTrue(success, "createDeal should revert with existing deal");
     }
 }
